788e4fba97a196a8c8add256debafafc6e3f5227
Valentino Vizner <valentino.vizner@gmail.com>
2020-01-20 22:18:00 +0100

    initial

---
+++ test_grader.py
@@ -0,0 +1,10 @@
+# USAGE
+# python test_grader.py --image images/test_01.png
+
+# import the necessary packages
+from imutils.perspective import four_point_transform
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
